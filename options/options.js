554aed8e48f0599788ef040e34421e94e59c7589
Crowdbotics <team@crowdbotics.com>
2024-03-01 00:51:43 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mortgage-insights-46737",
+  url: "https://mortgage-insights-46737.botics.co",
+  api: "https://mortgage-insights-46737.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
